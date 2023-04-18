ee86db6345cd9edf854bc38be2c559b5e795a8c5
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-04-18 19:10:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-# Imports go at the top
 from microbit import *
 from maqueen import *
 
-maqueen_robot = Maqueen()
-display.scroll(':)')
+
+def moverComoPeon():
+    robot.mover_celda()
+    
+def moverComoCaballo():
+    robot.mover_celda()
+    robot.mover_celda()
+    robot.mover_celda()
+    robot.girar_derecha()
+    robot.mover_celda()
+
+def moverComoTorre():
+    for i in range(1,8):
+     robot.mover_celda()
+
+robot = Maqueen()
 
 while True:
-    display.show(Image.HEART)
-    sleep(1000)
-   
-    maqueen_robot.forward()
-    utime.sleep(2)
-
-    maqueen_robot.turn_right()
-    utime.sleep(1)
-
-    maqueen_robot.turn_left()
-    utime.sleep(1)
-
-    maqueen_robot.stop()
+    if button_a.is_pressed():
+        display.show("C")
+        moverComoCaballo()
+        
+    elif button_b.is_pressed():
+        display.show("P")
+        moverComoPeon()
+        
+    else:
+        display.show(Image.HEART)
